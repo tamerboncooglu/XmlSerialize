cf656c69c3831e28df6a46ed2943ba5d523db0b8
Tamer Boncooglu <tamerboncooglu@gmail.com>
2013-01-15 13:06:13 +0200

    Added

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDirector = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(107, 27);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(332, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(195, 199);
+            this.listBox1.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(107, 53);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name";
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(107, 79);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 20);
+            this.txtYear.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Year";
+            // 
+            // txtDirector
+            // 
+            this.txtDirector.Location = new System.Drawing.Point(107, 105);
+            this.txtDirector.Name = "txtDirector";
+            this.txtDirector.Size = new System.Drawing.Size(100, 20);
+            this.txtDirector.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Director";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(107, 279);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Şifre";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(107, 253);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Email";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 345);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDirector);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDirector;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Settings settings = new Settings();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            settings = DeserializeFromXML();
+            foreach (var movie in settings.Movies)
+            {
+                listBox1.Items.Add(movie.Title);
+            }
+
+            txtEmail.Text = settings.UserInfo.Email;
+            txtPassword.Text = settings.UserInfo.Password;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var movie = new Movie()
+                {
+                    Director = txtDirector.Text,
+                    Name = txtName.Text,
+                    Title = txtTitle.Text,
+                    Year = txtYear.Text
+                };
+
+            settings.Movies.Add(movie);
+
+            settings.UserInfo = new UserInfo
+                {
+                    Email = txtEmail.Text,
+                    Password = txtPassword.Text
+                };
+
+            SerializeToXML(settings);
+
+            MessageBox.Show("Kayıt Oluşturuldu");
+
+            txtDirector.Clear();
+            txtName.Clear();
+            txtTitle.Clear();
+            txtYear.Clear();
+
+            Form1_Load(null, null);
+        }
+
+        public class Settings
+        {
+            public List<Movie> Movies { get; set; }
+            public UserInfo UserInfo { get; set; }
+        }
+
+        public class Movie
+        {
+            [XmlAttribute("Title")]
+            public string Title { get; set; }
+            [XmlAttribute("Name")]
+            public string Name { get; set; }
+            [XmlAttribute("Year")]
+            public string Year { get; set; }
+            [XmlAttribute("Director")]
+            public string Director { get; set; }
+        }
+
+        public class UserInfo
+        {
+            [XmlAttribute("Email")]
+            public string Email { get; set; }
+            [XmlAttribute("Password")]
+            public string Password { get; set; }
+        }
+
+        static public void SerializeToXML(Settings settings)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Settings));
+            StreamWriter writer = new StreamWriter(Application.StartupPath + "\\settings.xml");
+            serializer.Serialize(writer, settings);
+            writer.Close();
+            serializer = null;
+            writer.Dispose();
+            writer = null;
+            GC.Collect();
+        }
+
+        static Settings DeserializeFromXML()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Settings));
+            FileStream fs = new FileStream(Application.StartupPath + "\\settings.xml", FileMode.Open);
+            Settings settings = (Settings)deserializer.Deserialize(fs);
+            deserializer = null;
+            fs.Dispose();
+            fs = null;
+            GC.Collect();
+            return settings;
+        }
+    }
+}
